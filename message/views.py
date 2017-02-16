ebdf2bb6d0f43fe3076a357f66014b181181f44f
=asvito <alexandervirk@gmail.com>
2017-02-16 20:06:12 +0300

    New styles

--- message/views.py
+++ message/views.py
@@ -5,16 +5,16 @@ from django.core import serializers
 from models import User, Dialog
 from helpers import jsonfy_messages
 
-@login_required(login_url='/login')
+@login_required(login_url='/login/')
 def test(request):
     return HttpResponse("Test successeded.")
 
-@login_required(login_url='/login')
+@login_required(login_url='/login/')
 def dialogs(request):
     request.dialogs = request.user.dialogs.all()
     return render(request, 'index.html')
 
-@login_required(login_url='/login')
+@login_required(login_url='/login/')
 def messages(request, id):
     arr = request.user.dialogs.get(id=id).messages.all()
     return JsonResponse(jsonfy_messages(arr), safe=False)

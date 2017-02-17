e7e8d8ba33d467eafe79a2bb51ba3bcb50b10674
=asvito <asvito@mw2consulting.com>
2017-02-18 00:50:41 +0300

    Design udated

--- message/views.py
+++ message/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.core import serializers
 from models import User, Dialog
-from helpers import jsonfy_messages
 
 @login_required(login_url='/login/')
 def test(request):
@@ -17,4 +16,4 @@ def dialogs(request):
 @login_required(login_url='/login/')
 def messages(request, id):
     arr = request.user.dialogs.get(id=id).messages.all()
-    return JsonResponse(jsonfy_messages(arr), safe=False)
+    return JsonResponse([msg.as_json() for msg in arr], safe=False)
